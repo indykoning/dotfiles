fb03ba637c291c4947fb1cc19c4647684d654b5f
Indy Koning <indykoning@hotmail.com>
2019-11-19 20:46:03 +0100

    Automatic commit.

--- .dotfile_scripts/shutdown.sh
+++ .dotfile_scripts/shutdown.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-su -- indykoning -c "cd ~ && git add . && git commit -m\"Automatic commit.\" && git push origin master"
+su -- indykoning -c "cd ~ && git add . && git commit -m\"Automatic commit.\"; git push origin master"
