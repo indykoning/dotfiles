63709dee291cb6f25c8458f16b73df9ffe1b36f0
indy koning <indy@justbetter.nl>
2020-07-11 15:47:05 +0200

    Automatic commit.

--- .dotfile_scripts/shutdown.sh
+++ .dotfile_scripts/shutdown.sh
@@ -3,8 +3,5 @@ cmd="cd ~ && git add . && git commit -m\"Automatic commit.\"; git push origin ma
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     su -- indykoning -c "${cmd}"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "shutdown" > ~/.dotfile_scripts/shutdown.log
-    #echo $cmd
     bash -c "${cmd}"
-    echo "done" >> ~/.dotfile_scripts/shutdown.log
 fi
--- .dotfile_scripts/startup.sh
+++ .dotfile_scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cmd="cd ~ && git pull origin master"
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    su -- indykoning -c $cmd
+    su -- indykoning -c "${cmd}"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    bash -c $cmd
+    bash -c "${cmd}"
 fi
